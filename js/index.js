f108bed392e2e45c50d4a536e57874def2d5a835
antonkornilov-ua <83725472+antonkornilov-ua@users.noreply.github.com>
2022-07-29 22:57:40 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,62 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let boardLocked = false;
+let firstCard, secondCard;
+
+
+
+const flipCard = e => {
+    if (boardLocked) return;
+
+    const target = e.target.parentElement;
+
+    if (target === firstCard) return;
+
+    target.classList.add('flip');
+    if(!hasFlippedCard){
+        // first click
+        hasFlippedCard = true;
+        firstCard = target;
+    } else{
+        // second card
+        hasFlippedCard = false;
+        secondCard = target;
+        //  check for match
+        checkForMatch();
+    }
+};
+const checkForMatch = () => {
+    const isEqual = firstCard.dataset.framework === secondCard.dataset.framework;
+    isEqual ? disableCards() : unflipCards(); 
+}
+
+const disableCards = () => {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+};
+
+const unflipCards = () => {
+ boardLocked = true;
+        setTimeout(() => {           
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard()
+        }, 1000);
+}
+const resetBoard = () => {
+    // hardmode
+    [hasFlippedCard, boardLocked] = [false, false];
+    [firstCard, secondCard] = [null, null];
+    // Better
+//     hasFlippedCard = boardLocked = false;
+//     firstCard = secondCard = null;
+};
+
+cards.forEach(card => {
+    // add eventListener to every card
+    card.addEventListener("click", flipCard)
+    
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    card.style.order = randomIndex;
+});
